46da85ebf8d15d109acd18731f3b61041cdd9765
Siedrix <Siedrix@gmail.com>
2013-03-08 09:21:59 -0500

    Materiales

---
+++ 5-Dropdown/DropDown.js
@@ -0,0 +1,19 @@
+var DropDown = function(config){}
+
+DropDown._HTML = '\
+<div class="dropdown-container">\
+</div>\
+';
+
+DropDown.prototype._bindEvents = function() {}
+
+
+DropDown.prototype._renderDropdown = function() {};
+
+DropDown.prototype.setValue = function(value, label) {}
+
+DropDown.prototype.show = function(first_argument) {};
+
+DropDown.prototype.hide = function(first_argument) {};
+
+DropDown.prototype._position = function(){}
