b50d00af67e67d2a3786fe0fb2f55c05fe3635be
Siedrix <Siedrix@gmail.com>
2013-03-08 15:06:56 -0500

    Ejemplos al medio dia

--- 5-Dropdown/static_server.js
+++ 5-Dropdown/static_server.js
@@ -2,7 +2,7 @@ var http = require("http"),
     url = require("url"),
     path = require("path"),
     fs = require("fs")
-    port = process.argv[2] || 9000;
+    port = process.argv[2] || 8888;
 
 http.createServer(function(request, response) {
 
